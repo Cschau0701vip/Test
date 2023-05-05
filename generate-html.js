fad9cc5eef0358c4b01db4acd40daa855c4a81f0
Chetan Chaudhari <31230509+Cschau0701vip@users.noreply.github.com>
2023-05-05 19:29:33 +0530

    Update generate-html.js

--- generate-html.js
+++ generate-html.js
@@ -1,12 +1,10 @@
-const github1 = require('@actions/github');
-const octokit = new github1.GitHub(process.env.GITHUB_TOKEN, {baseUrl: 'https://github.com/api/v3'});
-const artifactUrl = github1.rest.actions.downloadArtifact({
+const artifactUrl = github.rest.actions.downloadArtifact({
   artifact_id: null,
   archive_format: 'zip'
 });
 const artifactLink = `[View Test Results](${artifactUrl})`;
 const commentBody = `Test results for this pull request: ${artifactLink}`;
-github1.rest.issues.createComment({
+github.rest.issues.createComment({
   issue_number: context.issue.number,
   body: commentBody
 });
